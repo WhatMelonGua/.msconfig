7cded4b837ddcfb4ab8e62135a579eea772bfba7
Vater Hu <92420346+WhatMelonGua@users.noreply.github.com>
2024-03-10 15:14:05 +0800

    Update exporter.sh

--- .msconfig/startup/load/exporter.sh
+++ .msconfig/startup/load/exporter.sh
@@ -2,7 +2,7 @@ exporter.PATH(){
   # export PATH
   local value=($(csv.query -i ${MS_CONFIG_HOME}/PATH.csv -s value -R))
   # init PATH
-  if [ ${PATH_DEFAULT}"x" != "x" ]; then
+  if [ "${PATH_DEFAULT}x" != "x" ]; then
     PATH=${PATH_DEFAULT}
   fi
   # append path to PATH
